2b3e0bea38583f2b599330f704900b1b3bd89f9a
azu <azuciao@gmail.com>
2021-09-26 12:05:00 +0900

    use MEMORY_NOTE_TOKEN

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { createGitHubProjectStorage } from "./note/adapters/GitHubProject";
 import { createCloudflareStorage } from "./note/adapters/cloudflare";
 import { render } from "./widget/render";
 
-declare var INPUT_YOUR_MEMORY_NOTE_TOKEN: string;
+declare var MEMORY_NOTE_TOKEN: string;
 declare var BACKEND_SERVICE: "github" | "cloudflare";
 declare var GITHUB_OWNER: string;
 declare var GITHUB_REPO: string;
@@ -29,7 +29,7 @@ const memoryNote = createMemoryNote({
 
 const Auth: Handler<{ token: string }> = (req, res) => {
     const token = req.query.get("token");
-    if (token !== INPUT_YOUR_MEMORY_NOTE_TOKEN) {
+    if (token !== MEMORY_NOTE_TOKEN) {
         res.send(400);
     }
 };
